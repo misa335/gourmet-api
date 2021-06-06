c693d0164d40eb863683cffc95709320c4207f66
Misako Kondoh <kondoh.misako@gmail.com>
2021-06-06 18:03:27 +0900

    add visited table

--- db/migrations/20210606083522_createTable.js
+++ db/migrations/20210606083522_createTable.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('visited', (table) => {
+        table.increments();
+        table.string('name').notNullable();
+        table.string('menu').notNullable();
+        table.date('date');
+        table.integer('rate');
+        table.string('favorite');
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+    return knex.schema.hasTable('visited')
+        .then(function(exists) {
+            if(exists) {
+                return knex.schema.dropTable('visited');
+            }
+        });
 };
