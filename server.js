08ae44707f1e288c9440dd0db71e9580d8f33da0
Misako Kondoh <kondoh.misako@gmail.com>
2021-06-07 10:21:13 +0900

    add APIs for wishlist

--- server.js
+++ server.js
@@ -21,12 +21,14 @@ app.listen(port, () => {
 
 //endpoints
 //visited table
-//get
+//get all list
 app.get('/logs', async (req, res) => {
     await knex.select().from('visited')
         .then(datas => res.send(datas))
         .catch(err => console.log('error:', err));
 });
+
+//get single list
 
 //post
 app.post('/logs', async (req, res) => {
@@ -36,7 +38,7 @@ app.post('/logs', async (req, res) => {
         menu: req.body.menu,
         genre: req.body.genre,
         rate: req.body.rate,
-        comment: req.body.rate,
+        comment: req.body.comment,
         lat: req.body.lat,
         lng: req.body.lng
     })
@@ -54,7 +56,7 @@ app.put('/logs/:id', async (req, res) => {
             menu: req.body.menu,
             genre: req.body.genre,
             rate: req.body.rate,
-            comment: req.body.rate,
+            comment: req.body.comment,
             lat: req.body.lat,
             lng: req.body.lng
         })
@@ -72,30 +74,74 @@ app.delete('/logs/:id', async (req, res) => {
         .catch(err => console.log('error:', err));
 });
 
-//wishlist table
-//get
 
+//wishlist table
+//get all wishlist
+app.get('/wish', async (req, res) => {
+    await knex.select().from('wishlist')
+        .then(datas => res.send(datas))
+        .catch(err => console.log('error:', err));
+});
+
+//get single wishlist
 
 //post
+app.post('/wish', async (req, res) => {
+    await knex('wishlist').insert({
+        image: req.body.image,
+        name: req.body.name,
+        genre: req.body.genre,
+        station: req.body.station,
+        lat: req.body.lat,
+        lng: req.body.lng
+    })
+    .then(() => knex.select().from('wishlist'))
+    .then((datas) => res.send(datas))
+    .catch(err => console.log('error:', err));
+});
 
 //put
+app.put('/wish/:id', async (req, res) => {
+    await knex('wishlist').where('id', req.params.id)
+        .update({
+            image: req.body.image,
+            name: req.body.name,
+            genre: req.body.genre,
+            station: req.body.staion,
+            lat: req.body.lat,
+            lng: req.body.lng
+        })
+        .then(() => knex.select().from('wishlist'))
+        .then(datas => res.send(datas))
+        .catch(err => console.log('error:', err));
+});
 
 //delete
+app.delete('/wish/:id', async (req, res) => {
+    await knex('wishlist').where('id', req.params.id)
+        .del()
+        .then(() => knex.select().from('wishlist'))
+        .then(datas => res.send(datas))
+        .catch(err => console.log('error:', err));
+});
+
 
 //favorite table
-//get
+//get all favorite list
 app.get('/favs', async (req, res) => {
     await knex.select().from('favorite')
         .then(datas => res.send(datas))
         .catch(err => console.log('error:', err));
 });
 
+//get single list
+
 //post
 app.post('/favs', async (req, res) => {
     await knex('favorite').insert({
         image: req.body.image,
         name: req.body.name,
-        comment: req.body.rate,
+        comment: req.body.comment,
         genre: req.body.genre,
         lat: req.body.lat,
         lng: req.body.lng
@@ -111,7 +157,7 @@ app.put('/favs/:id', async (req, res) => {
         .update({
             image: req.body.image,
             name: req.body.name,
-            comment: req.body.rate,
+            comment: req.body.comment,
             genre: req.body.genre,
             lat: req.body.lat,
             lng: req.body.lng
